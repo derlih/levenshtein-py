a7868dfb7e21a4d56eab152f387cc52d18f98932
Dmitry Erlikh <derlih@gmail.com>
2024-07-01 20:51:38 +0200

    increase iterations in bench

--- benchmark.py
+++ benchmark.py
@@ -2,7 +2,7 @@ from timeit import timeit
 
 if __name__ == "__main__":
     print("Benchmark:")
-    iterations = 10_000
+    iterations = 1_000_000
     full_matrix = timeit(
         setup="from levenshtein_py import full_matrix",
         stmt="full_matrix('Levenshtein', 'Frankenstein')",
