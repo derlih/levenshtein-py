ad7693ae61aef66c9c361fc7480992cad6cbc513
Dmitry Erlikh <derlih@gmail.com>
2024-07-03 17:51:37 +0200

    benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,11 +1,55 @@
+from dataclasses import dataclass
 from timeit import timeit
+from typing import Tuple
+
+ITERATIONS = 100_000
+S1 = "Levenshtein"
+S2 = "Frankenstein"
+
+
+@dataclass
+class PackageToTest:
+    pkg: str
+    setup: str
+    call: str
+
 
 if __name__ == "__main__":
-    print("Benchmark:")
-    iterations = 1_000_000
-    full_matrix = timeit(
-        setup="from levenshtein_py import full_matrix",
-        stmt="full_matrix('Levenshtein', 'Frankenstein')",
-        number=iterations,
+    print(f"Benchmark ({ITERATIONS} iterations):")
+
+    packages: Tuple[PackageToTest, ...] = (
+        PackageToTest(
+            "levenshtein_py",
+            "from levenshtein_py import full_matrix",
+            f"full_matrix('{S1}', '{S2}')",
+        ),
+        PackageToTest(
+            "Levenshtein",
+            "from Levenshtein import distance",
+            f"distance('{S1}', '{S2}')",
+        ),
+        PackageToTest(
+            "python-Levenshtein",
+            "from Levenshtein import distance",
+            f"distance('{S1}', '{S2}')",
+        ),
+        PackageToTest(
+            "leven",
+            "from leven import levenshtein",
+            f"levenshtein('{S1}', '{S2}')",
+        ),
+        PackageToTest(
+            "pylev",
+            "from pylev import levenshtein",
+            f"levenshtein('{S1}', '{S2}')",
+        ),
     )
-    print(f"Full matrix({iterations}): {full_matrix/iterations} sec")
+
+    for pkg_to_test in packages:
+        result = timeit(
+            setup=pkg_to_test.setup,
+            stmt=pkg_to_test.call,
+            number=ITERATIONS,
+        )
+
+        print(f"{pkg_to_test.pkg}: {result/ITERATIONS} sec")
