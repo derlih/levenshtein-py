dd47c3d59a633444de9d343808c124b0bea0be50
Dmitry Erlikh <derlih@gmail.com>
2024-07-03 19:34:48 +0200

    cythonize

--- pdm_build.py
+++ pdm_build.py
@@ -1,15 +1,16 @@
 from pathlib import Path
+from typing import Any, Dict
 
-from mypyc.build import mypycify
+from Cython.Build import cythonize
+from pdm.backend.hooks import Context
 
 
-def pdm_build_hook_enabled(context):
+def pdm_build_hook_enabled(context: Context) -> None:
     return context.target == "wheel"
 
 
-def pdm_build_update_setup_kwargs(context, setup_kwargs):
-    src_dir = Path(__file__).parent / "src" / "levenshtein_py"
-    files_to_mypyc = [str(f) for f in src_dir.glob("*.py")]
+def pdm_build_update_setup_kwargs(context: Context, setup_kwargs: Dict[str, Any]):
+    src = Path(__file__).parent / "src" / "levenshtein_py" / "two_rows.py"
     setup_kwargs.update(
-        ext_modules=mypycify(["--strict", *files_to_mypyc], verbose=True)
+        ext_modules=cythonize(str(src), compiler_directives={"language_level": "3"})
     )
