d883ef66b22981829066f8360ce32b29a163b712
Dmitry Erlikh <d.erlikh@smartrecruiters.com>
2024-08-15 16:04:04 +0200

    build different variants

--- pdm_build.py
+++ pdm_build.py
@@ -1,5 +1,7 @@
 from typing import Any, Dict
 
+from Cython.Build import cythonize
+from mypyc.build import mypycify
 from pdm.backend.hooks import Context
 from setuptools import Extension
 
@@ -11,9 +13,24 @@ def pdm_build_hook_enabled(context: Context) -> None:
 def pdm_build_update_setup_kwargs(
     context: Context, setup_kwargs: Dict[str, Any]
 ) -> None:
-    sources = [
-        "src/levenshtein_py/native.c",
-    ]
+    native_ext = Extension(
+        name="levenshtein_py.native",
+        sources=["src/levenshtein_py/native.c"],
+    )
+    cython_exts: list[Extension] = cythonize(
+        [
+            Extension(
+                name="levenshtein_py.cython",
+                sources=["src/levenshtein_py/wagner_fischer.py"],
+            )
+        ]
+    )
+    mypyc_exts = mypycify(["src/levenshtein_py/mypyc.py"])
+
     setup_kwargs.update(
-        ext_modules=[Extension(name="levenshtein_py.native", sources=sources)]
+        ext_modules=[
+            native_ext,
+            *cython_exts,
+            *mypyc_exts,
+        ]
     )
---
+++ src/levenshtein_py/mypyc.py
@@ -0,0 +1,1 @@
+src/levenshtein_py/wagner_fischer.py
