f7d74a3ed219aa556ba562c86c3a656d2f44da85
Dmitry Erlikh <derlih@gmail.com>
2024-08-18 19:04:10 +0200

    fix

--- src/levdist/__init__.py
+++ src/levdist/__init__.py
@@ -1,13 +1,17 @@
+import typing
+
 from .classic import classic
-from .wagner_fischer import wagner_fischer
+
+if typing.TYPE_CHECKING:
+    levenshtein: typing.Callable[[str, str], int]
 
 try:
     from .native import wagner_fischer_native  # type: ignore
 
-    def levenshtein(a: str, b: str) -> int:
-        return wagner_fischer_native(a, b)  # type: ignore[no-any-return]
-
+    levenshtein = wagner_fischer_native
 except ImportError:
+    from .wagner_fischer import wagner_fischer
+
     levenshtein = wagner_fischer
 
 __all__ = ["classic", "levenshtein", "wagner_fischer"]
