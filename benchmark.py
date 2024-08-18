fdda18cb5c9655f26277276223b8aeb6b554057e
Dmitry Erlikh <derlih@gmail.com>
2024-08-18 15:40:25 +0200

    benchmark (#32)
    * add typer
    
    * add typer to vs spellcheck
    
    * add md support of benchmark script
    
    * generate benchmark md
    
    * add link to bench md to readme

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,10 @@
+import platform
+import sys
 from dataclasses import dataclass
+from enum import Enum
 from timeit import timeit
-from typing import Tuple
+
+import typer
 
 ITERATIONS = 1_000_000
 S1 = "Levenshtein"
@@ -8,58 +12,99 @@ S2 = "Frankenstein"
 DISTANCE = 6
 
 
-@dataclass
+@dataclass(frozen=True)
 class PackageToTest:
-    pkg: str
+    name: str
     setup: str
     call: str
 
 
-if __name__ == "__main__":
-    print(f"Benchmark ({ITERATIONS} iterations):")
+PACKAGES = (
+    PackageToTest(
+        "levenshtein_py",
+        "from levenshtein_py import levenshtein",
+        f"levenshtein('{S1}', '{S2}')",
+    ),
+    PackageToTest(
+        "Levenshtein",
+        "from Levenshtein import distance",
+        f"distance('{S1}', '{S2}')",
+    ),
+    PackageToTest(
+        "python-Levenshtein",
+        "from Levenshtein import distance",
+        f"distance('{S1}', '{S2}')",
+    ),
+    PackageToTest(
+        "leven",
+        "from leven import levenshtein",
+        f"levenshtein('{S1}', '{S2}')",
+    ),
+    PackageToTest(
+        "pylev",
+        "from pylev import levenshtein",
+        f"levenshtein('{S1}', '{S2}')",
+    ),
+)
 
-    packages: Tuple[PackageToTest, ...] = (
-        PackageToTest(
-            "levenshtein_py",
-            "from levenshtein_py import levenshtein",
-            f"levenshtein('{S1}', '{S2}')",
-        ),
-        PackageToTest(
-            "Levenshtein",
-            "from Levenshtein import distance",
-            f"distance('{S1}', '{S2}')",
-        ),
-        PackageToTest(
-            "python-Levenshtein",
-            "from Levenshtein import distance",
-            f"distance('{S1}', '{S2}')",
-        ),
-        PackageToTest(
-            "leven",
-            "from leven import levenshtein",
-            f"levenshtein('{S1}', '{S2}')",
-        ),
-        PackageToTest(
-            "pylev",
-            "from pylev import levenshtein",
-            f"levenshtein('{S1}', '{S2}')",
-        ),
+
+@dataclass(frozen=True)
+class BenchmarkResult:
+    pkg: str
+    duration: float
+
+
+class OutputFormat(str, Enum):
+    TEXT = "txt"
+    MARKDOWN = "md"
+
+
+def benchmark(pkg: PackageToTest) -> BenchmarkResult:
+    exec(
+        f"""
+{pkg.setup}
+assert {pkg.call} == {DISTANCE}
+             """
     )
 
-    # Check correctness
-    for pkg_to_test in packages:
-        exec(
-            f"""
-{pkg_to_test.setup}
-assert {pkg_to_test.call} == {DISTANCE}
-             """
-        )
+    result = timeit(
+        setup=pkg.setup,
+        stmt=pkg.call,
+        number=ITERATIONS,
+    )
 
-    for pkg_to_test in packages:
-        result = timeit(
-            setup=pkg_to_test.setup,
-            stmt=pkg_to_test.call,
-            number=ITERATIONS,
-        )
+    return BenchmarkResult(pkg.name, result)
 
-        print(f"{pkg_to_test.pkg}: {result/ITERATIONS} sec")
+
+def main(
+    output: typer.FileTextWrite = typer.Argument(sys.stdout),
+    format: OutputFormat = typer.Option(OutputFormat.TEXT),
+) -> None:
+    with typer.progressbar(PACKAGES) as pkgs:
+        results = tuple((benchmark(pkg) for pkg in pkgs))
+
+    if format == OutputFormat.TEXT:
+        for result in results:
+            typer.echo(f"{result.pkg}: {result.duration / ITERATIONS} sec", output)
+    elif format == OutputFormat.MARKDOWN:
+        typer.echo(f"# Benchmark ({ITERATIONS} iterations)", output)
+        typer.echo(file=output)
+
+        typer.echo("| OS | CPU | Python |", output)
+        typer.echo("| -- | --- | ------ |", output)
+        typer.echo(
+            f"| {platform.system()} {platform.release()} | {platform.processor()} | {sys.version} |",
+            output,
+        )
+        typer.echo(
+            """
+| Package | Duration of one iteration |
+| ------- | ------------------------- |""",
+            output,
+        )
+        for result in results:
+            typer.echo(f"| {result.pkg} | {result.duration} |", output)
+
+
+if __name__ == "__main__":
+    typer.run(main)
